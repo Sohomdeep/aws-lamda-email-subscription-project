775f27689c16a0450ece8fed857cf0ca01cff7dc
Sohomdeep <sohomdeeppaul@gmail.com>
2022-12-02 13:31:28 +0530

    file fetch from s3 using api

---
+++ handler/getQuotes.js
@@ -0,0 +1,36 @@
+const AWS = require('aws-sdk');
+AWS.config.update({region: process.env.REGION})
+const s3 = new AWS.S3();
+
+module.exports.getQuotes = (event, context, callback) => {
+    console.log("Incomming-->", event);
+
+    //access json from s3
+    s3.getObject({
+        Bucket: "aws-lambda-email-subscription-project-bucket",
+        Key: "quotes.json"
+    },
+        function(err, data){
+            if(err){
+                console.error(err);
+                callback(new Error(err));
+                return;
+            }else{
+                var json = JSON.parse(data.body)
+                console.log("JSON---->", json);
+
+                //create a response
+                const response = {
+                    headers: {
+                        "Content-Type": "application/json",
+                        "Access-Control-Allow-Methods": "*",
+                        "Access-Control-Allow-Origin": "*"
+                    },
+                    statusCode: 200,
+                    body: JSON.stringify(json)
+                }
+                callback(null, response)
+            }
+        }
+    )
+}
